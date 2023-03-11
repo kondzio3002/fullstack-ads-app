c019d7b730e16cc432c59048952bca4a15818bb8
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-11 22:14:07 +0100

    Add user model

---
+++ models/user.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  login: { type: String, require: true },
+  password: { type: String, require: true },
+  avatar: { type: String, require: true },
+  phone: { type: String, require: true }
+});
+
+module.exports = mongoose.model('User', userSchema);
