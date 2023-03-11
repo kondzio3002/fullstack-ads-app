15635016cf26e27385de0a2e32fab8992f31b809
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-11 22:03:59 +0100

    Connect to datebase

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const socket = require('socket.io');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const NODE_ENV = process.env.NODE_ENV;
+let dbURI = '';
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -11,6 +16,22 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-const server = app.listen(8000, () => {
+if (NODE_ENV === 'production') dbURI = `mongodb+srv://kondzio3002:${process.env.DB_PASS}@annoucement-app.gjodigx.mongodb.net/AnnoucementAppDB?retryWrites=true&w=majority`;
+else if (NODE_ENV === 'test') dbURI = 'mongodb://localhost:27017/AnnoucementAppDBtest'
+else dbURI = 'mongodb://localhost:27017/AnnoucementAppDB';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the datebase');
+});
+db.on('error', err => console.log('Error ' + err));
+
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running!');
 });
+
+const io = socket(server);
+
+io.on('connection', (socket) => {});
