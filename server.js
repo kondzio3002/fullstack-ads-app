f9b9a278fa80b05b5ceb06e39ef767839c6858cd
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-12 14:34:07 +0100

    Add auth routes

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/auth', require('./routes/auth.routes'));
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
