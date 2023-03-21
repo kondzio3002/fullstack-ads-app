fac937c630b781670dd22885aff0e070f74eef95
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-21 20:05:43 +0100

    Add NavBar

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -1,20 +1,8 @@
-import { Nav, Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import NavBar from "../NavBar/NavBar";
 
 const Header = () => {
   return (
-    <Navbar bg='secondary' variant='dark' className='py-2 p-5 rounded'>
-      <Navbar.Brand href='/'>Ads App</Navbar.Brand>
-      <Navbar.Collapse className='justify-content-end'>
-        <Nav>
-          <Nav.Link as={NavLink} to='/'><Button color='secondary'>Home</Button></Nav.Link>
-          <Nav.Link as={NavLink} to='/register'><Button color='warning'>Sign up</Button></Nav.Link>
-          <Nav.Link as={NavLink} to='/login'><Button color='success'>Sign in</Button></Nav.Link>
-          <Nav.Link as={NavLink} to='/logout'><Button color='danger'>Sign out</Button></Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <NavBar />
   );
 };
 
