aceb480f249f9216634c4cf1e61dbcc55758cbff
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-26 23:29:34 +0200

    Add delete for ad

--- client/src/components/features/Ad/Ad.js
+++ client/src/components/features/Ad/Ad.js
@@ -1,17 +1,38 @@
 import styles from "./Ad.module.scss";
-import { useSelector } from "react-redux";
-import { Navigate, useParams } from "react-router-dom";
-import { IMG_URL } from "../../../config";
-import { getAdById, getUserId } from "../../../redux/dataRedux";
-import { Button, Col, Row } from "reactstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { IMG_URL, API_URL } from "../../../config";
+import { getAdById, getUserId, loadAds, loadAdsRequest } from "../../../redux/dataRedux";
+import { Button, Col, Row, Modal } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
 
 
 const Ad = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { id } = useParams();
   const ad = useSelector(state => getAdById(state, id));
   const user = useSelector(getUserId);
+
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
+  const handleRemoveAd = e => {
+    e.preventDefault();
+
+    const options = {
+      method: 'DELETE',
+      credentials: 'include',
+    };
+    fetch(`${API_URL}/ads/${ad._id}`, options)
+    loadAds();
+
+    toggle();
+    navigate('/');
+  };
+
 
   if(!ad) return <Navigate to='/' />
   else return (
@@ -20,10 +41,26 @@ const Ad = () => {
 
         { user.id === ad.user._id &&
           <div className={`mt-4 ${styles.adButtons}`}>
-            <Button color='secondary' className='mx-3'>Edit</Button>
-            <Button color='danger'>Delete</Button>
+            <Button variant='secondary' className='mx-3'>Edit</Button>
+            <Button variant='danger' onClick={toggle}>Delete</Button>
           </div>
         }
+
+        <Modal show={modal} onHide={toggle}>
+          <Modal.Header closeButton>
+            <Modal.Title>Are you sure?</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            This operation will completely remove this post from the app.<br></br>
+            Are you sure you want to do that?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={toggle}>
+              Cancel
+            </Button>
+            <Button variant="danger" onClick={handleRemoveAd}>Remove</Button>
+          </Modal.Footer>
+        </Modal>
 
         <h2 className='mt-3'>{ad.title}</h2>
 
